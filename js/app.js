12ea8b23e39196d37609e04bd171182c3ce9d12d
rafalsky94 <rafalbatorowicz94@gmail.com>
2017-08-20 20:45:09 +0200

    wersja 1.5

--- js/app.js
+++ js/app.js
@@ -1,12 +1,26 @@
 $(()=> {
 
+    let swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        effect: 'coverflow',
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: 'auto',
+        coverflow: {
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows : true
+        },
+        autoplay: 2000,
+        autoplayDisableOnInteraction: false
+    });
    let navbar = $(".navbar");
-
-
 
    $(window).on("scroll", () => {
 
-       if (scrollY >= 900) {
+       if (scrollY >= 840) {
            navbar.css("backgroundColor", "#63181f");
            console.log("ok");
        } else {
